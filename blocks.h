676c918c1e88340ab32a9fcf90c8e0cf6281d7a3
Dylrex <23338296+Dylrex@users.noreply.github.com>
2021-10-03 00:46:00 +1000

    dwmblocks

---
+++ blocks.h
@@ -0,0 +1,16 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+
+	{"", "dwm-spotify",					1,		14},
+	{"", "pacupdates",					3600,		8},
+	{"", "dwm-news",					1800,		13},
+	{"", "dwm-mailbox",					1800,		12},
+	{"", "dwm-weather",					3600,		11},
+	{"", "dwm-volume",					60,		10},
+	{"", "dwm-clock",					60,		0},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " ";
+static unsigned int delimLen = 5;
