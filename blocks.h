1748fdc70e827bc1b8698c278e027ac1f4b41301
Dylrex <23338296+Dylrex@users.noreply.github.com>
2021-10-17 04:27:31 +1100

    icons/scripts

--- blocks.h
+++ blocks.h
@@ -2,15 +2,16 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 
-	{"", "dwm-spotify",					1,		14},
-	{"", "pacupdates",					3600,		8},
-	{"", "dwm-news",					1800,		13},
-	{"", "dwm-mailbox",					1800,		12},
-	{"", "dwm-weather",					3600,		11},
+	{"󰓇", "dwm-spotify",					1,		14},
+	{"󰚰", "pacupdates",					3600,		8},
+	{"󰎕", "dwm-news",					1800,		13},
+	{"󰇮", "dwm-mailbox",					1800,		12},
+	/* {"", "dwm-netuse",					1,		9}, */
+	/* {"", "dwm-weather",					3600,		11}, */
 	{"", "dwm-volume",					30,		10},
 	{"", "dwm-clock",					5,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " ";
+static char delim[] = " | ";
 static unsigned int delimLen = 5;
