7da36700361ca5b28989a5467e5b3facababea37
dylrex <git@dylrex.com>
2022-06-12 01:55:02 +1000

    icons

--- blocks.h
+++ blocks.h
@@ -2,10 +2,10 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 
-	{"󰓇", "dwm-spotify",					1,		14},
-	{"󰚰", "pacupdates",					3600,		8},
-	{"󰎕", "dwm-news",					1800,		13},
-	{"󰇮", "dwm-mailbox",					1800,		12},
+	{"", "dwm-spotify",					1,		14},
+	{"", "pacupdates",					3600,		8},
+	{"", "dwm-news",					1800,		13},
+	/* {"󰇮", "dwm-mailbox",					1800,		12}, */
 	/* {"", "dwm-netuse",					1,		9}, */
 	/* {"", "dwm-weather",					3600,		11}, */
 	{"", "dwm-volume",					30,		10},
